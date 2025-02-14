b96bf668b3616594b60985b8037b209d7d2ad8f7
Trevor <trevor.c.pettit@gmail.com>
2025-02-14 13:26:37 -0700

    prettier is running

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,57 @@
 <script lang="ts">
-import { RouterLink, RouterView } from "vue-router"
-import LoginView from "./views/login/LoginView.vue"
+import { RouterLink, RouterView } from 'vue-router'
+import LoginView from './views/login/LoginView.vue'
 
 export default {
-	name: "App",
-	components: {
-		LoginView,
-		RouterLink,
-		RouterView
-	},
-	data: function () {
-		return {
-			isAuthenticated: false
-		}
-	},
-	mounted() {
-		console.log("mounted")
-	},
-	beforeCreate() {
-		console.log("beforeCreate")
-	},
-	beforeMount() {
-		console.log("beforeMount")
-	},
-	created() {
-		console.log("created")
-		if (this.isAuthenticated) {
-			this.$router.push("/home") //if authenticated, send to the home page
-		}
-	},
-	methods: {
-		logout() {
-			this.isAuthenticated = false
-		},
-		checkAuth(auth) {
-			console.log("Authenticated!", auth)
-			this.isAuthenticated = auth
-		}
-	}
+    name: 'App',
+    components: {
+        LoginView,
+        RouterLink,
+        RouterView,
+    },
+    data: function () {
+        return {
+            isAuthenticated: false,
+        }
+    },
+    mounted() {
+        console.log('mounted')
+    },
+    beforeCreate() {
+        console.log('beforeCreate')
+    },
+    beforeMount() {
+        console.log('beforeMount')
+    },
+    created() {
+        console.log('created')
+        if (this.isAuthenticated) {
+            this.$router.push('/home') //if authenticated, send to the home page
+        }
+    },
+    methods: {
+        logout() {
+            this.isAuthenticated = false
+        },
+        checkAuth(auth) {
+            console.log('Authenticated!', auth)
+            this.isAuthenticated = auth
+        },
+    },
 }
 </script>
 
 <template>
-	<div v-if="isAuthenticated">
-		<header>
-			<div class="header-navigation">
-				<RouterLink to="/">Home</RouterLink>
-				<RouterLink to="/about">About</RouterLink>
-			</div>
-			<button @click="logout()">Logout</button>
-		</header>
+    <div v-if="isAuthenticated">
+        <header>
+            <div class="header-navigation">
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </div>
+            <button @click="logout()">Logout</button>
+        </header>
 
-		<RouterView />
-	</div>
-	<LoginView
-		v-else
-		:is-authenticated="isAuthenticated"
-		@authenticate="checkAuth($event)"
-	/>
+        <RouterView />
+    </div>
+    <LoginView v-else :is-authenticated="isAuthenticated" @authenticate="checkAuth($event)" />
 </template>
